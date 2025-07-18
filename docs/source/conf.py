b8b5004a0465bfa0ba03125a8ff010574c9d9b43
Leonardo Benicio <parquet-rubbles-08@icloud.com>
2025-07-18 12:47:28 -0300

    Update conf.py
    Signed-off-by: Leonardo Benicio <parquet-rubbles-08@icloud.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # These folders are copied to the documentation's HTML output
 html_static_path = ['_static']
